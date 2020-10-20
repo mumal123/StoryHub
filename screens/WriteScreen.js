2d07f60e825d3053a4c6a1518990d85c4b182ee1
Mumal Swami <66372736+mumal123@users.noreply.github.com>
2020-10-20 15:53:30 +0530

    Update WriteScreen.js

--- screens/WriteScreen.js
+++ screens/WriteScreen.js
@@ -1,69 +1,112 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+
+    import React from 'react';
+import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Header } from 'react-native-elements';
-export default class WriteStory extends React.Component {
+import db from '../config';
+import firebase from 'firebase';
+
+export default class WriteStoryScreen extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: '',
+            author: '',
+            summary: '',
+        }
+    }
+
+    submitStory = () => {
+        db.collection('stories').add({
+            'title': this.state.title,
+            'author': this.state.author,
+            'summary': this.state.summary,
+        })
+        this.setState({
+            title: '',
+            author: '',
+            summary: ''
+        })
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Header backgroundColor={'#9c8210'}
-          centerComponent={{
-                    text: 'Story',
-                    style: { color: '#fff', fontSize: 20, width:50 },
-                }} />
-                <View style={styles.titleView}>
-                    <TextInput placeholder="Story Title"></TextInput>
-                </View>
-                <View style={styles.authorView}>
-                    <TextInput placeholder="Author"></TextInput>
-                </View>
-                <View style={styles.storyView}>
-                    <TextInput placeholder="Write your story"></TextInput>
-                </View>
-                <TouchableOpacity style={styles.buttonView}>
-                    <Text>SUBMIT</Text>
+            
+            <View>
+                <Text style={{ textAlign: 'center', backgroundColor: "pink", paddingBottom: 20, paddingTop: 20 }}>Story Hub</Text>
+                <TextInput
+                    style={styles.textInput1}
+                    placeholder={"Story Name"}
+                    onChangeText={(text) => {
+                        this.setState({
+                            title: text
+                        })
+                    }}
+                    value={this.state.title} />
+                <TextInput
+                    style={styles.textInput2}
+                    placeholder={"Author"}
+                    onChangeText={(text) => {
+                        this.setState({
+                            author: text
+                        })
+                    }}
+                    value={this.state.author} />
+                <TextInput
+                    style={styles.storyView}
+                    placeholder={"Summary"}
+                    onChangeText={(text) => {
+                        this.setState({
+                            summary: text
+                        })
+                    }}
+                    value={this.state.summary} />
+                <TouchableOpacity style={styles.submit} onPress={this.submitStory}>
+                    <Text style={styles.buttonText}>Submit</Text>
                 </TouchableOpacity>
-
+                
             </View>
-        );
+                
+        )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#fff',
-        alignItems: 'center',
+    textInput1: {
+        borderWidth: 3,
+        marginTop: 130,
+        width: 450,
         justifyContent: 'center',
+        alignSelf: 'center',
+        textAlign: 'center'
     },
-    titleView: {
-        marginTop: 10,
-        borderColor: "black",
-        borderWidth: 1,
-        borderRadius: 5,
-        width: 350
-    },
-    authorView: {
-        marginTop: 10,
-        borderColor: "black",
-        borderWidth: 1,
-        borderRadius: 5,
-        width: 350
+    textInput2: {
+        borderWidth: 3,
+        marginTop: 100,
+        width: 450,
+        justifyContent: 'center',
+        alignSelf: 'center',
+        textAlign: 'center'
     },
     storyView: {
         marginTop: 10,
         borderColor: "black",
+        justifyContent: 'center',
+        alignSelf: 'center',
+        textAlign: 'center',
         borderWidth: 1,
         borderRadius: 5,
         width: 350,
         height: 200
     },
-    buttonView: {
-        backgroundColor: "red",
-        marginTop: 20,
-        borderRadius: 5,
-        width: 80,
-        height: 40,
-        alignItems: "center",
-        justifyContent: "center"
+    submit: {
+        borderWidth: 2,
+        alignSelf: 'center',
+        width: 90,
+        height: 33,
+        marginTop: 70,
+        backgroundColor: "pink"
+    },
+    buttonText: {
+        textAlign: 'center'
     }
-});
+})
